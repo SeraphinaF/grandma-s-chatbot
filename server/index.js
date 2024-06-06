cfb36c5bbf5550c1e4188793a8fe887b5ce1c90d
SeraphinaF <1028267@hr.nl>
2024-06-06 19:17:28 +0200

    changed position cors

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ import cors from "cors";
 import { ChatOpenAI } from "@langchain/openai"
 
 dotenv.config()
-
-// const corsOptions = {
-//     origin: 'https://grandma-s-chatbot-client-c6rxelenj.vercel.app/', // Allow only your frontend domain
-//     optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-//   };
-  
-//   app.use(cors(corsOptions));
 const app = express();
+
+const corsOptions = {
+    origin: 'https://grandma-s-chatbot-client-c6rxelenj.vercel.app/', // Allow only your frontend domain
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+  
+  app.use(cors(corsOptions));
 
 const model = new ChatOpenAI({
     azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
