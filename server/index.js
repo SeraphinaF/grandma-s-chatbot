2ec135a3d80f4feb5042f806bd2366be68d7699d
SeraphinaF <1028267@hr.nl>
2024-06-11 11:03:16 +0200

    console.log quote

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ async function getQuote() {
 
         const data = await response.json();
         const quote = data.contents.quotes[0].quote;
-        console.log(data)
+        console.log(quote)
         return quote;
     } catch (error) {
         console.error('Error fetching the quote:', error);
